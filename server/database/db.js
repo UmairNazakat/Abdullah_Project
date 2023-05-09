12d8aaf668d56debf19dc129885fbbeb5f1c55f8
UmairNazakat <umairnazakat2222@gmail.com>
2023-05-09 18:17:49 +0500

    abdullah bhai cred

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 
 const connection = async (username, userpassword) => {
-const URL = `mongodb://${username}:${userpassword}@ac-d0ithy9-shard-00-00.8be5zle.mongodb.net:27017,ac-d0ithy9-shard-00-01.8be5zle.mongodb.net:27017,ac-d0ithy9-shard-00-02.8be5zle.mongodb.net:27017/WEATHERPRACTICE?ssl=true&replicaSet=atlas-jgmzac-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// const URL = `mongodb://${username}:${userpassword}@ac-d0ithy9-shard-00-00.8be5zle.mongodb.net:27017,ac-d0ithy9-shard-00-01.8be5zle.mongodb.net:27017,ac-d0ithy9-shard-00-02.8be5zle.mongodb.net:27017/WEATHERPRACTICE?ssl=true&replicaSet=atlas-jgmzac-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const URL = `mongodb+srv://abdullahsiraj95:abdullahsiraj95@mefa.s4s27wc.mongodb.net/`;
 
     try{
         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser : true});
